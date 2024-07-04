1e9962f420bb900b07224410a92730a17f5602bf
Seth Mwebi <sethmwebi27@gmail.com>
2024-07-04 18:16:04 +0300

    writes code for array map method

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  const titleCasedTutorials = tutorials.map((tutorial) =>
+    tutorial.replace(/\b\w/g, (l) => l.toUpperCase()),
+  );
+  return titleCasedTutorials;
+};
